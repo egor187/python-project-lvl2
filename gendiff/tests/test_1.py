85e0e3cbc41f0e1fcb534901349b796a02a3af1e
Егор <oegor8194@gmail.com>
2021-01-08 12:11:01 +0300

    add tests

---
+++ gendiff/tests/test_1.py
@@ -0,0 +1,9 @@
+from gendiff.generate_diff import generate_diff
+import pytest
+
+
+def test():
+    assert generate_diff(
+            '/home/egor_22_linux/python-project-lvl2/gendiff/tests/fixtures/file1.json', \
+                    '/home/egor_22_linux/python-project-lvl2/gendiff/tests/fixtures/file2.json'
+                    ) == open('./gendiff/tests/fixtures/test_1_res.txt', 'r').read()

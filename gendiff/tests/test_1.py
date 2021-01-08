db8a0d497e9f4ecf862d4ba1e1bab8e1b6621671
Егор <oegor8194@gmail.com>
2021-01-08 14:42:50 +0300

    fix path for test

--- gendiff/tests/test_1.py
+++ gendiff/tests/test_1.py
@@ -1,11 +1,18 @@
 from gendiff.generate_diff import generate_diff
 
 
+#def test():
+#    assert generate_diff(
+#            '/home/egor_22_linux/python-project-lvl2/'
+#            'gendiff/tests/fixtures/file1.json',
+#            '/home/egor_22_linux/python-project-lvl2'
+#            '/gendiff/tests/fixtures/file2.json') \
+#            == open('./gendiff/tests/fixtures/test_1_res.txt', 'r'
+#                    ).read()
+
 def test():
     assert generate_diff(
-            '/home/egor_22_linux/python-project-lvl2/'
-            'gendiff/tests/fixtures/file1.json',
-            '/home/egor_22_linux/python-project-lvl2'
-            '/gendiff/tests/fixtures/file2.json') \
+            './gendiff/tests/fixtures/file1.json',
+            './gendiff/tests/fixtures/file2.json') \
             == open('./gendiff/tests/fixtures/test_1_res.txt', 'r'
                     ).read()

8f1785f8fb2898c811915c4e84ad9652cce7506f
Егор <oegor8194@gmail.com>
2021-01-14 18:39:33 +0300

    change json-formatter

--- gendiff/formater/json.py
+++ gendiff/formater/json.py
@@ -1,6 +1,9 @@
 import json
 
 
+#def json_formatter(dict_):
+#    res = json.dumps(dict_, indent=2, sort_keys=True)
+#    return res + "\n"
+
 def json_formatter(dict_):
-    res = json.dumps(dict_, indent=2, sort_keys=True)
-    return res + "\n"
+    return json.dumps(dict_)
